cb21536f230b0e0cbfbc66786f44fdb4912d0b39
Chukwudi Derek Uche <cuche@purdue.edu>
2016-01-30 22:54:17 -0500

    User can be signed in

--- app/src/main/java/com/grubquest/grubquest_android/LoginActivity.java
+++ app/src/main/java/com/grubquest/grubquest_android/LoginActivity.java
@@ -1,17 +1,19 @@
 package com.grubquest.grubquest_android;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.firebase.client.AuthData;
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
 
 public class LoginActivity extends AppCompatActivity {
-    private AuthData authData;
     private Button login_button;
     private EditText password;
     private EditText username;
@@ -30,18 +32,37 @@ public class LoginActivity extends AppCompatActivity {
         final String email = username.getText().toString();
         final String pass = password.getText().toString();
 
+        firebase.createUser("cuche@purdue.edu", "chukwudi", new Firebase.ResultHandler() {
+            @Override
+            public void onSuccess() {
+                System.out.println("Yes");
+            }
+
+            @Override
+            public void onError(FirebaseError firebaseError) {
+                System.out.println(firebaseError.getDetails());
+                System.out.println(firebaseError.getMessage());
+                System.out.println("No");
+            }
+        });
+
         login_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 firebase.authWithPassword(email, pass, new Firebase.AuthResultHandler() {
                     @Override
                     public void onAuthenticated(AuthData authData) {
-                        System.out.println("Signed in");
+                        //send to next activity with AuthData in Bundle
+                        Intent next = new Intent(LoginActivity.this, HomeActivity.class);
+                        startActivity(next);
                     }
 
                     @Override
                     public void onAuthenticationError(FirebaseError firebaseError) {
-                        System.out.println("Error");
+                        Log.d("GrubQuest", firebaseError.getMessage());
+                        Log.d("GrubQuest", firebaseError.getDetails());
+                        Toast.makeText(LoginActivity.this, "Something fucked up", Toast.LENGTH_LONG)
+                                .show();
                     }
                 });
             }
